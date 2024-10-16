b8228b0992946546777b2417eeb2c2189e163f59
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-16 11:46:39 +0600

    Added Controller and Views for Categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "categories/index"
+  get "categories/show"
+  get "categories/new"
+  get "categories/edit"
+  get "categories/destroy"
   get "users/index"
   get "users/show"
   get "users/new"
