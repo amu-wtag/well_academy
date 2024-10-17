dc1899edeb8f5acbbf03a49018ac48227e95f959
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-17 09:43:33 +0600

    Added unique constraint to name and not null to description of Category model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_17_021039) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_17_034013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,9 +44,10 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_17_021039) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", limit: 100, null: false
-    t.text "description"
+    t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
   create_table "courses", force: :cascade do |t|
