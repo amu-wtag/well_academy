2a775fd993b757c79ac8d075b1ee8bb2370e633c
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-17 14:44:26 +0600

    Added validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,5 +8,9 @@ class Course < ApplicationRecord
   enum :level, { beginner: 0, intermediate: 1, advanced: 2 }
 
   validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
   validates :level, inclusion: { in: levels.keys }
+  validates :language, presence: true
+  validates :duration, presence: true
 end
