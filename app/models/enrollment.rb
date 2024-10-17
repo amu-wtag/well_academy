a558f7033483ee3ddac5aad4bd568fadd96d7d14
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-17 14:33:56 +0600

    removed deprecated enum declaration

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -2,7 +2,7 @@ class Enrollment < ApplicationRecord
   belongs_to :student, class_name: "User"
   belongs_to :course
 
-  enum completion_status: { not_started: 0, in_progress: 1, completed: 2 }
+  enum :completion_status, { not_started: 0, in_progress: 1, completed: 2 }
 
   validates :progress, numericality: { greater_than_or_equal_to: 0.0, less_than_or_equal_to: 1.0 }
 end
