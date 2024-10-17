8b81951fd359d7ffe29993aa1d518f09bd35b599
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-17 15:47:13 +0600

    Update enrollment.rb

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -8,5 +8,5 @@ class Enrollment < ApplicationRecord
   validates :course_id, presence: true
   validates :enrolled_at, presence: true
   validates :progress, numericality: { greater_than_or_equal_to: 0.0, less_than_or_equal_to: 1.0 }
-  validates :completion_status, inclusion: { in: %w[not\ started in\ progress completed] }
+  validates :completion_status, inclusion: { in: completion_statuses.keys }
 end
