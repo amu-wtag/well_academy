97680e9e73742a37e456094fd7696d422cae827c
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-20 11:34:00 +0600

    Added test for model Category.

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,4 +1,6 @@
 FactoryBot.define do
   factory :category do
+    name { "Programming" }
+    description { "Courses related to programming languages." }
   end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,28 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:category) { FactoryBot.build(:category) }
+
+  describe 'validations' do
+    it 'is not valid without a name' do
+      category.name = nil
+      expect(category).not_to be_valid
+    end
+
+    it 'is not valid without a description' do
+      category.description = nil
+      expect(category).not_to be_valid
+    end
+
+    it 'is not valid with a duplicate name' do
+      FactoryBot.create(:category) # Create the first category (stores it in database)
+      duplicate_category = FactoryBot.build(:category) # Attempt to create a duplicate
+      expect(duplicate_category).not_to be_valid
+    end
+
+    it 'is not valid if name is too long (greater than 100)' do
+      category.name = 'a' * 101  # Assuming the maximum length is 100
+      expect(category).not_to be_valid
+    end
+  end
 end
