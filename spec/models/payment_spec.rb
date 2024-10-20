1f22afaf4589c8ae88e8463c83f927ae5322d46c
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-20 11:35:38 +0600

    Added test for model Payment

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Payment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:payment) { build(:payment) }
+
+  describe "validations" do
+    it 'is valid with valid attributes' do
+      expect(payment).to be_valid
+    end
+
+    it 'is not valid without a course price' do
+      payment.course_price = nil
+      expect(payment).not_to be_valid
+    end
+
+    it 'is not valid without a user' do
+      payment.user = nil
+      expect(payment).not_to be_valid
+    end
+
+    it 'is not valid without a course' do
+      payment.course = nil
+      expect(payment).not_to be_valid
+    end
+  end
+
+  describe "enums" do
+    it 'defines payment types' do
+      expect(Payment.payment_types.keys).to include("bkash", "brac_bank")
+    end
+
+    it 'defines payment statuses' do
+      expect(Payment.statuses.keys).to include("unpaid", "paid")
+    end
+  end
 end
