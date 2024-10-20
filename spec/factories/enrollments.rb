ef5574e1859d4d1893c544078b141ffab40a6e47
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-20 11:34:48 +0600

    Added test for model Enrollments

--- spec/factories/enrollments.rb
+++ spec/factories/enrollments.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :enrollment do
-    
+    student { association :user, role: 'student' }
+    course
+    enrolled_at { Time.now }
+    progress { 0.0 }
+    completion_status { 'not_started' }
   end
 end
--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -1,5 +1,44 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Enrollment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:enrollment) { create(:enrollment) }
+
+  # Validations
+  describe 'validations' do
+    it 'is not valid without a student' do
+      enrollment.student = nil
+      expect(enrollment).not_to be_valid
+    end
+
+    it 'is not valid without a course' do
+      enrollment.course = nil
+      expect(enrollment).not_to be_valid
+    end
+
+    it 'is not valid without an enrolled_at timestamp' do
+      enrollment.enrolled_at = nil
+      expect(enrollment).not_to be_valid
+    end
+
+    it 'is valid with progress defaulting to 0.0' do
+      expect(enrollment.progress).to eq(0.0)
+    end
+
+    it 'is not valid with progress less than 0.0 or greater than 1.0' do
+      enrollment.progress = -0.1
+      expect(enrollment).not_to be_valid
+
+      enrollment.progress = 1.1
+      expect(enrollment).not_to be_valid
+    end
+
+    it 'is not valid with an invalid completion_status' do
+      expect(Enrollment.completion_statuses.key?('finished')).to be_falsey
+    end
+
+    it 'is valid with valid completion_status' do
+      enrollment.completion_status = 'in_progress'
+      expect(enrollment).to be_valid
+    end
+  end
 end
