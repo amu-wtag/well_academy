2324b8ef1ae51b70b0212137112227e829126299
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-20 11:34:59 +0600

    Added test for model Lesson

--- spec/factories/lessons.rb
+++ spec/factories/lessons.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :lesson do
-    
+    title { "Lesson 1: Introduction" }
+    order { 1 }
+    course
   end
 end
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Lesson, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:lesson) { create(:lesson) }
+
+  describe 'validations' do
+    it 'is not valid without a title' do
+      lesson.title = nil
+      expect(lesson).not_to be_valid
+    end
+
+    it 'is not valid without an order' do
+      lesson.order = nil
+      expect(lesson).not_to be_valid
+    end
+
+    it 'is not valid without a course' do
+      lesson.course = nil
+      expect(lesson).not_to be_valid
+    end
+  end
 end
