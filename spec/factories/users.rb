7be04c55451bbd640f06854162715f5d958fafa4
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-20 14:50:39 +0600

    Update users.rb factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) { |n| "user#{n}@example.com" } # generates unique emails for each user instance.
+    sequence(:name) { |n| "user#{n}" }
+    sequence(:email) { |n| "user#{n}@example.com" }
     password { "password" }
-    name { "Test User" }
     phone { "1234567890" }
     date_joined { Time.now }
     bio { "A test bio" }
