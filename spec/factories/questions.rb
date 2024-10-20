a461682f97c80b9dbf1e9c9c0ad721c9572b61d2
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-20 11:35:59 +0600

    Added test for model Question

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :question do
-    
+    content { "What is the capital of France?" }
+    marks { 5 }
+    quiz
+
+    created_at { Time.current }
+    updated_at { Time.current }
   end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:question) { build(:question) }
+
+  describe "validations" do
+    it 'is valid with valid attributes' do
+      expect(question).to be_valid
+    end
+
+    it 'is not valid without content' do
+      question.content = nil
+      expect(question).not_to be_valid
+    end
+
+    it 'is not valid without marks' do
+      question.marks = nil
+      expect(question).not_to be_valid
+    end
+
+    it 'is not valid without a quiz' do
+      question.quiz = nil
+      expect(question).not_to be_valid
+    end
+
+    it 'has a valid factory' do
+      expect(build(:question)).to be_valid
+    end
+  end
 end
