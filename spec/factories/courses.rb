d92fc4320049f8df1596b784bb00fbe09ebff89b
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-20 11:34:30 +0600

    Added test for model Course

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -1,4 +1,14 @@
 FactoryBot.define do
   factory :course do
+    title { "Learn Ruby on Rails" }
+    description { "A comprehensive course on Ruby on Rails." }
+    association :teacher, factory: :user, role: :teacher
+    category
+    price { 99.99 }
+    level { 'beginner' }
+    language { "English" }
+    duration { 120 } # 2 hours
+    created_at { Time.now }
+    updated_at { Time.now }
   end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:course) { create(:course) }
+
+  describe 'validations' do
+    it 'is not valid without a title' do
+      course.title = nil
+      expect(course).not_to be_valid
+    end
+
+    it 'is not valid without a description' do
+      course.description = nil
+      expect(course).not_to be_valid
+    end
+
+    it 'is not valid without a teacher' do
+      course.teacher = nil
+      expect(course).not_to be_valid
+    end
+
+    it 'is not valid without a category' do
+      course.category = nil
+      expect(course).not_to be_valid
+    end
+
+    it 'is not valid without a price' do
+      course.price = nil
+      expect(course).not_to be_valid
+    end
+
+    it 'is not valid without a language' do
+      course.language = nil
+      expect(course).not_to be_valid
+    end
+
+    it 'is not valid without a category' do
+      course.category = nil
+      expect(course).not_to be_valid
+    end
+
+    it 'is not valid without a duration' do
+      course.duration = nil
+      expect(course).not_to be_valid
+    end
+  end
 end
