385c6b6ffb760265083b007d87556e38ae4ee8a6
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-20 11:36:46 +0600

    Added test for model QuizParticipation

--- spec/models/quiz_participation_spec.rb
+++ spec/models/quiz_participation_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe QuizParticipation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:quiz_participation) { build(:quiz_participation) }
+
+  describe "validations" do
+    it 'is valid with valid attributes' do
+      expect(quiz_participation).to be_valid
+    end
+
+    it 'is not valid without marks obtained' do
+      quiz_participation.marks_obtained = nil
+      expect(quiz_participation).not_to be_valid
+    end
+
+    it 'is not valid without total marks' do
+      quiz_participation.total_marks = nil
+      expect(quiz_participation).not_to be_valid
+    end
+
+    it 'is not valid without a student' do
+      quiz_participation.student = nil
+      expect(quiz_participation).not_to be_valid
+    end
+
+    it 'is not valid without a quiz' do
+      quiz_participation.quiz = nil
+      expect(quiz_participation).not_to be_valid
+    end
+  end
 end
