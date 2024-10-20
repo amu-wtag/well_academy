6f48f0aa890b96fda03fb376d5d2b9dcad048ea8
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-20 11:37:07 +0600

    Added test for model Review

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :review do
-    
+    course
+    association :student, factory: :user
+    rating { 5 }
+    comment { "This course was excellent!" }
+
+    created_at { Time.current }
+    updated_at { Time.current }
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:review) { build(:review) }
+
+  describe "validations" do
+    it 'is valid with valid attributes' do
+      expect(review).to be_valid
+    end
+
+    it 'is not valid without a course' do
+      review.course = nil
+      expect(review).not_to be_valid
+    end
+
+    it 'is not valid without a student' do
+      review.student = nil
+      expect(review).not_to be_valid
+    end
+
+    it 'is not valid without a rating' do
+      review.rating = nil
+      expect(review).not_to be_valid
+    end
+
+    it 'is not valid with a rating less than 1' do
+      review.rating = 0
+      expect(review).not_to be_valid
+    end
+
+    it 'is not valid with a rating greater than 5' do
+      review.rating = 6
+      expect(review).not_to be_valid
+    end
+
+    it 'has a valid factory' do
+      expect(build(:review)).to be_valid
+    end
+  end
 end
