ed3fa4d7c8813309789dfee06423b8d1f322a10e
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-20 11:35:23 +0600

    Added test for model Option

--- spec/factories/options.rb
+++ spec/factories/options.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :option do
-    
+    option_text { "Sample option text" }
+    is_correct { false }
+    question
   end
 end
--- spec/models/option_spec.rb
+++ spec/models/option_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Option, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:option) { create(:option) }
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(option).to be_valid
+    end
+
+    it 'is not valid without option_text' do
+      option.option_text = nil
+      expect(option).not_to be_valid
+    end
+
+    it 'is not valid without a question' do
+      option.question = nil
+      expect(option).not_to be_valid
+    end
+
+    it 'is not valid without is_correct being a boolean' do
+      option.is_correct = nil
+      expect(option).not_to be_valid
+    end
+  end
+
+  # Associations
+  describe 'associations' do
+    it 'belongs to a question' do
+      expect(option.question).to be_a(Question)
+    end
+  end
 end
