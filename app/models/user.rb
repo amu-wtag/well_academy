a1ff075e9d2e95661d757508963be90b1b2a8255
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-22 10:39:59 +0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,13 @@
 class User < ApplicationRecord
-  has_secure_password  # For password hashing and authentication
+  has_secure_password
 
   has_one_attached :profile_picture
-  has_many_attached :certificates
+  has_many_attached :student_certificates
+  has_many_attached :teacher_certificates
 
   enum role: %i[student teacher admin].freeze
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :role, inclusion: { in: roles.keys }
-
-  def pic_resize(x, y)
-    x = x.to_i
-    y = y.to_i
-    # puts "\n\n ***Converted x: #{x}, Converted y: #{y}" # Debugging line
-
-    profile_picture.variant(resize_to_limit: [ x, y ])
-  end
 end
