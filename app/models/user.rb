3be7d0b94c961d747bfd463bcb6cccb3936dce7c
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-23 11:42:07 +0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-  has_secure_password  # For password hashing and authentication
+  has_secure_password
 
   has_one_attached :profile_picture
-  has_many_attached :certificates
+  has_many_attached :student_certificates
+  has_one_attached :grad_certificate
+  has_one_attached :postgrad_certificate
 
-  enum :role, { student: 0, teacher: 1, admin: 2 }
+  enum role: %i[student teacher admin].freeze
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
