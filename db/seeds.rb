f5305b5166b285da8390e21a51b4d3e331890d6a
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-24 16:36:52 +0600

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-# User.destroy_all
-
 User.create!(
   name: "Kaium Uddin",
   email: "admin@a.com",
@@ -8,3 +6,8 @@ User.create!(
   bio: "Admin",
   role: "admin"
 )
+
+User.create!(name: "Hasib Chy", email: "hasib@example.com", password: "1234", phone: "1234567891", bio: "Demo user", role: "student")
+User.create!(name: "Samin Wasee", email: "wasee@example.com", password: "1234", phone: "1234567892", bio: "Demo user", role: "student")
+User.create!(name: "Redwan Ahmed", email: "redwan@example.com", password: "1234", phone: "1234567893", bio: "Demo user", role: "student")
+User.create!(name: "Arnab", email: "arnab@example.com", password: "1234", phone: "1234567894", bio: "Demo user", role: "student")
