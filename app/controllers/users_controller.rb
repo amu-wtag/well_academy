b0bad19589e1a57007796ec7f6b2b7f30ad0ea33
Md. Ahnaf Muttaqi Chowdhury <ahnaf.muttaqi@welldev.io>
2024-10-24 16:36:22 +0600

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,12 @@ class UsersController < ApplicationController
 
   load_and_authorize_resource
 
-  before_action :set_user, only: %i[index show edit update destroy]
+  before_action :set_user, only: %i[index show edit update destroy become_teacher pending]
+  before_action :set_pending_users, only: %i[pending]
+  after_action :set_pending_users, only: %i[reject_teacher]
 
   def index
-    # @users = User.all
-    @users = User.where.not(role: "admin")
+    @users = User.where.not(role: "admin").order(:name)
   end
 
   def show
@@ -24,24 +25,32 @@ class UsersController < ApplicationController
       flash[:notice] = "User created successfully."
       redirect_to root_path
     else
-      render :new
+      flash.now[:alert] = "Email already exists!"
+      render :new, status: :unprocessable_entity
     end
   end
 
   def edit
     @tmp_user = User.find(params[:id])
-    # puts "**debug** This is edit and tmp_user: #{@tmp_user.name}"
   end
 
   def update
+    # puts "**debug:  #{action_name}"
+    referer = request.referer
+    puts "**debug:  #{referer}"
+
     @tmp_user = User.find(params[:id])
     # puts "**debug** This is update and tmp_user: #{@tmp_user.name}"
     if @tmp_user.update(user_params)
       flash[:notice] = "User updated successfully."
-      redirect_to user_path
+      if @tmp_user == @user
+        redirect_to user_path
+      else
+        redirect_to users_path
+      end
     else
       flash.now[:alert] = "Empty/Invalid fields!"
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -49,9 +58,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user.destroy
-    flash[:notice] = "User destroyed successfully."
-    redirect_to root_path
+    @tmp_user = User.find(params[:id])
+    @tmp_user.destroy
+    flash[:notice] = "User deleted successfully."
+    redirect_to users_path
   end
 
   def confirm
@@ -61,7 +71,8 @@ class UsersController < ApplicationController
       @user.update(confirmed_at: Time.now, confirmation_token: nil)
       redirect_to @user, notice: "Your email has been confirmed successfully!"
     else
-      redirect_to root_path, alert: "Invalid confirmation token or email already confirmed."
+      flash.now[:alert] = "Invalid confirmation token or email already confirmed."
+      redirect_to root_path, status: :unprocessable_entity
     end
   end
 
@@ -69,17 +80,53 @@ class UsersController < ApplicationController
   end
 
   def become_teacher
-    @user = User.find(session[:user_id])
   end
 
+  def pending
+  end
+
+  def approve_teacher
+    @tmp_user = User.find(params[:id])
+
+    @tmp_user.role = :teacher
+    if @tmp_user.save
+      flash[:notice] = "Teacher approved"
+      redirect_to pending_users_path
+    else
+      flash.now[:alert] = "Couldn't approve teacher"
+      render :pending, status: :unprocessable_entity
+    end
+  end
+
+  def reject_teacher
+    @tmp_user = User.find(params[:id])
+
+    @tmp_user.grad_certificate.purge
+    @tmp_user.postgrad_certificate.purge
+
+    flash.now[:alert] = "User rejected successfully!"
+    render :pending, status: :unprocessable_entity
+  end
+
+  def remove_profile_picture
+    @tmp_user = User.find(params[:id])
+    @tmp_user.profile_picture.purge
+
+    flash.now[:alert] = "Profile picture removed successfully!"
+    render :show, status: :unprocessable_entity
+  end
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :phone, :date_joined, :bio, :role, :profile_picture, student_certificates: [], teacher_certificates: [])
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :phone, :date_joined, :bio, :role, :profile_picture, :grad_certificate, :postgrad_certificate, student_certificates: [])
   end
 
   def set_user
     @user = User.find_by(id: session[:user_id])
     @tmp_user = @user
+  end
+
+  def set_pending_users
+    @users = User.all.select { |user| user.grad_certificate.attached? && user.student? }
   end
 end
